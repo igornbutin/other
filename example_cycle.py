7941dfc73917568d74594b39d36f6a7ecb571cdb
igornbutin <igornbutin@gmail.com>
2023-03-06 16:42:27 +0600

    Add example_cycle.py

---
+++ example_cycle.py
@@ -0,0 +1,17 @@
+urls = ["http://example.com/page1", "http://example.com/page2", "http://example.com/page3"]
+current_index = 0
+
+while True:
+    # выбираем текущий URL и обрабатываем его
+    current_url = urls[current_index]
+    print("Обработка URL:", current_url)
+    
+    # увеличиваем индекс для выбора следующего URL
+    current_index += 1
+    
+    # если индекс стал больше или равен длине массива, начинаем с начала
+    if current_index >= len(urls):
+        current_index = 0
+        
+    # ждем некоторое время перед обработкой следующего URL
+    time.sleep(5)
